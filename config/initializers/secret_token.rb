3f6cd7293d94a08bf1333021564d92b99c472e86
Shikhov Miron <light-arrow@mail.ru>
2011-06-13 12:28:39 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SynApp::Application.config.secret_token = '1d18b63ad85526a97feb54e9651b17f6abedeeadcc593ce1d012939b44a4b11f1d6e7d39c9b8bd0b3a51d7171ab6d82266d96ec1c4d9d23c1f462858f3cb2786'
